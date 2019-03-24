6ee83d494c7566a7974eaabe8b02bae617babc05
George Gotsidis <48317082+ggotsidis@users.noreply.github.com>
2019-03-24 16:20:04 +0200

    Menu Test with four activities

---
+++ app/src/main/java/com/example/menu_example_4screens/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.menu_example_4screens;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SwitchCompat;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbarMain);
+        setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        switch(id) {
+            case R.id.action_settings:
+               Intent intent_settings = new Intent(MainActivity.this, Settings.class);
+               startActivity(intent_settings);
+                break;
+            case R.id.action_about:
+                Intent intent_about = new Intent(MainActivity.this, About.class);
+                startActivity(intent_about);
+                break;
+            case R.id.action_help:
+                Intent intent_help = new Intent(MainActivity.this, Help.class);
+                startActivity(intent_help);
+                break;
+
+            default:
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
